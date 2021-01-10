09a80b7724ee46e42ad873e3b4e4072db17f539b
aokll <pastuhov_1992@mail.ru>
2021-01-10 23:01:42 +0300

    dz2

---
+++ lev3_les2/chat/entity/User.java
@@ -0,0 +1,31 @@
+package com.chat.entity;
+
+public class User {
+    private int id;
+    private String nickname;
+    private String email;
+    private String password;
+
+    public User(int id, String nickname, String email, String password) {
+        this.id = id;
+        this.nickname = nickname;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
